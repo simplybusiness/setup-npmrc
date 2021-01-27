566c5a405852bfdc9cd5e2b1a5232229d443271e
Leslie Hoare <leslie.hoare@simplybusiness.co.uk>
2021-01-27 19:58:23 +0000

    Use config from inputs

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const fs = require("fs");
 const core = require("@actions/core");
 
-const makeNpmrc = (token) => `
-@simplybusiness:registry=https://bnw-registry.simplybusiness.io
+const makeNpmrc = (registry, scope, token) => `
+@${scope}:registry=${registry}
 always-auth=true
-email=pest@simplybusiness.co.uk
 _auth=${token}
 `;
 
 try {
+  const registry = core.getInput("registry");
+  const scope = core.getInput("scope");
   const token = core.getInput("token");
-  const contents = makeNpmrc(token);
+  const contents = makeNpmrc(registry, scope, token);
   fs.writeFileSync(".npmrc", contents);
 } catch (error) {
   core.setFailed(error.message);
