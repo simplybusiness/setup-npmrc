2df755def75181ab1b1358bddc0ab8e0524fc510
Leslie Hoare <leslie.hoare@simplybusiness.co.uk>
2021-01-27 17:21:56 +0000

    Add action code

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("todo");
+const fs = require("fs");
+const core = require("@actions/core");
+
+const makeNpmrc = (token) => `
+@simplybusiness:registry=https://bnw-registry.simplybusiness.io
+always-auth=true
+email=pest@simplybusiness.co.uk
+_auth=${token}
+`;
+
+try {
+  const token = core.getInput("token");
+  const contents = makeNpmrc(token);
+  fs.writeFileSync(".npmrc", contents);
+} catch (error) {
+  core.setFailed(error.message);
+}
