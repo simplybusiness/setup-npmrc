5121c4b11cc6464095af413e32d211afc1a29cd1
Gregorio Setti <gregorio.setti@simplybusiness.co.uk>
2021-01-28 14:48:32 +0000

    Cleanup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 const core = require("@actions/core");
 
 const makeNpmrc = (registry, scope, token) => `
@@ -13,7 +14,10 @@ try {
   const token = core.getInput("token");
   const writePath = core.getInput("write-path") || process.cwd();
   const contents = makeNpmrc(registry, scope, token);
-  fs.writeFileSync(`${writePath}.npmrc`, contents);
+  const filePath = path.join(writePath, '.npmrc');
+  fs.writeFileSync(filePath, contents);
+  console.log(`Writing to ${filePath}`);
+  core.exportVariable('NPM_CONFIG_USERCONFIG', filePath);
 } catch (error) {
   core.setFailed(error.message);
 }
